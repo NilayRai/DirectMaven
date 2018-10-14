0982d69eb7c21dbf93d8dec5f85b4035e77425a4
NilayRai <40742612+NilayRai@users.noreply.github.com>
2018-10-14 18:37:37 +0530

    Update LoginServlet.java

--- src/test/java/com/servlet/demo/LoginServlet.java
+++ src/test/java/com/servlet/demo/LoginServlet.java
@@ -2,6 +2,6 @@ package com.servlet.demo;
 
 public class LoginServlet {
 	public static void main(String[] args) {
-		System.out.println("Hello JAva");
+		System.out.println("Hello JAva"+"Nilay");
 	}
 }
